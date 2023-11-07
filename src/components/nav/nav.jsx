1889c106e67f363ab0e234c751358df8e94d602a
suryam-s <suryamsourya8@gmail.com>
2023-11-07 18:03:51 +0530

    nav jsx fix

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {RiServiceLine} from 'react-icons/ri'
-import {AiOutlineProject} from 'react-icons/ai'
-import {IoIosContacts} from 'react-icons/io'
-import {useState} from 'react'
+import React, { useState } from 'react';
+import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { RiServiceLine } from 'react-icons/ri';
+import { AiOutlineProject } from 'react-icons/ai';
+import { IoIosContacts } from 'react-icons/io';
 
 const Nav = () => {
-  const[activeNav,setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState('home'); // Use meaningful values
+
   return (
     <nav>
-      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href="#skills"onClick={()=> setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><RiServiceLine/></a>
-      <a href="#project"onClick={()=> setActiveNav('#project')} className={activeNav === '#project' ? 'active' : ''}><AiOutlineProject/></a>
-      <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><IoIosContacts/></a>
+      <button onClick={() => setActiveNav('home')} className={activeNav === 'home' ? 'active' : ''}>
+        <AiOutlineHome />
+      </button>
+      <button onClick={() => setActiveNav('about')} className={activeNav === 'about' ? 'active' : ''}>
+        <AiOutlineUser />
+      </button>
+      <button onClick={() => setActiveNav('skills')} className={activeNav === 'skills' ? 'active' : ''}>
+        <RiServiceLine />
+      </button>
+      <button onClick={() => setActiveNav('project')} className={activeNav === 'project' ? 'active' : ''}>
+        <AiOutlineProject />
+      </button>
+      <button onClick={() => setActiveNav('contact')} className={activeNav === 'contact' ? 'active' : ''}>
+        <IoIosContacts />
+      </button>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
